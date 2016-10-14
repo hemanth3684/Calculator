336af44fe5a5ad42808d038ad9357823b1f5185f
hemanth3684 <hemanth.tadikonda@aspiresys.com>
2016-10-14 14:56:17 +0530

    Update CalcEngine.cs

--- CalcEngine.cs
+++ CalcEngine.cs
@@ -151,7 +151,7 @@ namespace Calculator
                 {
                     m_decimal = true;
                 }
-               	if( m_input.Contains( "-" ) ? m_sign = "-" : m_sign = "+" )
+               	m_sign = ( m_input.Contains( "-" ) ) ? "-" : "+";
                 //if (m_input.Contains("-"))
                 //{
                 //    m_sign = "-";
